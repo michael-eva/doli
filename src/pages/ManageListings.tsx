4a38f06262dd54e9c63819707c1c33f21ae1324a
galactic-shark <evamichael100@gmail.com>
2024-01-29 10:52:56 +0800

    updated listings to display only listings by user

--- src/pages/ManageListings.tsx
+++ src/pages/ManageListings.tsx
@@ -37,34 +37,15 @@ export default function ManageListings() {
             setIsLoading(false)
         }
     }, [user?.id])
-    // const getPosts = async () => {
-    //     const { error, data } = await supabase
-    //         .from("posts")
-    //         .select("*")
-    //         .eq("id", user?.id)
+    console.log(user);
 
-    //     if (error) {
-    //         return console.error(error);
-    //     }
-    //     const parsedData = data?.map((post) => {
-
-    //         return {
-    //             ...post,
-    //             selectedTags: JSON.parse(post.selectedTags).map((tag: any) => tag),
-    //             openingHours: JSON.parse(post.openingHours).map((tag: any) => tag)
-    //         };
-    //     });
-
-
-    //     setPosts(parsedData);
-
-    // }
     const getCombinedData = async () => {
         try {
             // Fetch posts data
             const { data: postsData, error: postsError } = await supabase
                 .from("posts")
                 .select("*")
+                .eq("id", user?.id)
 
             if (postsError) {
                 console.error("Error fetching posts data:", postsError);
@@ -72,6 +53,8 @@ export default function ManageListings() {
 
             if (postsData) {
                 // Process posts data
+                console.log(postsData);
+
                 const parsedPostsData = postsData.map((post) => ({
                     ...post,
                     selectedTags: JSON.parse(post.selectedTags).map((tag: any) => tag),
@@ -81,7 +64,8 @@ export default function ManageListings() {
                 // Fetch locations data
                 const { data: locationsData, error: locationsError } = await supabase
                     .from("locations")
-                    .select("*");
+                    .select("*")
+                // .eq("userId", user?.id)
 
                 if (locationsError) {
                     console.error("Error fetching locations data:", locationsError);
@@ -115,15 +99,23 @@ export default function ManageListings() {
     }
 
     const cardsEl = () => {
-        return (
-            <div className="flex flex-wrap gap-4 max-w-7xl m-auto justify-start">
-                {posts.map((item: CardProps) => {
-                    return (
-                        <div key={item.postId} className="mt-10">
-                            <Card {...item} showStatus={true} onDelete={deletePost} />
-                        </div>
-                    );
-                })}
+        if (posts?.length > 0) {
+
+            return (
+                <div className="flex flex-wrap gap-4 max-w-7xl m-auto justify-start">
+                    {posts.map((item: CardProps) => {
+                        return (
+                            <div key={item.postId} className="mt-10">
+                                <Card {...item} showStatus={true} onDelete={deletePost} />
+                            </div>
+                        );
+                    })}
+                </div>
+            )
+        } else return (
+            <div className=" flex items-center text-2xl flex-col pt-10">
+                <p >No listings to display.</p>
+                <p >Post a listing to view it here.</p>
             </div>
         )
     }
