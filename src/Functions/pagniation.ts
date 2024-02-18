3550293aa2950a5dede7f1e197dde0c03a214a8c
galactic-shark <evamichael100@gmail.com>
2024-02-18 16:36:48 +0800

    fixed pagination

--- src/Functions/pagniation.ts
+++ src/Functions/pagniation.ts
@@ -4,13 +4,6 @@ function paginatePage(currentPage: number, pageSize: number, filterPosts: CardPr
     const startIndex = (currentPage - 1) * pageSize;
     const endIndex = startIndex + pageSize;
     const paginatePage = filterPosts.slice(startIndex, endIndex);
-    // console.log("paginate page:", paginatePage);
-    console.log("filter posts:", filterPosts);
-    console.log("end index:", endIndex);
-    console.log("current page", currentPage);
-
-
-
     return paginatePage;
 }
 
