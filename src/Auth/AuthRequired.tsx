6e1afe979c380afa114ea4955457577fb32ef2ed
galactic-shark <evamichael100@gmail.com>
2024-02-20 10:01:22 +0800

    added netlify env variables

--- src/Auth/AuthRequired.tsx
+++ src/Auth/AuthRequired.tsx
@@ -1,12 +1,8 @@
 import { Navigate, Outlet } from "react-router";
 
 export default function AuthRequired() {
-    // production
-    const token = localStorage.getItem(import.meta.env.VITE_REACT_APP_LOGIN_TOKEN);
-    // const token = localStorage.getItem("sb-yagpsuctumdlmcazzeuv-auth-token");
 
-    // development
-    // const token = localStorage.getItem("sb-awkmxabdskcgxkzpqiru-auth-token");
+    const token = localStorage.getItem(import.meta.env.VITE_REACT_APP_LOGIN_TOKEN);
     if (!token) {
         return (
             <Navigate to={"/login"}
