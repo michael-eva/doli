860ba53c78690ccfc9a9e5c19d9ea2a8a0dd0210
galactic-shark <evamichael100@gmail.com>
2024-04-03 19:44:14 +0800

    refactor

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -84,54 +84,56 @@ export default function Home() {
                 name="doli"
                 type="website" />
             <div className=" max-w-7xl md:mx-auto pb-10">
-                {isMobile &&
-                    <div className=" flex flex-col gap-5">
-                        <div className=" rounded flex flex-col items-center mt-6">
-                            <h1 className=" text-3xl font-fira_sans" style={{ color: "#0097B2" }}>Hungry? Thirsty?</h1>
-                            <p className=" text-xl leading-10 max-w-xs mt-3">If you want to know the best places to eat and drink...  <span className=" font-bold text-2xl" style={{ color: "#CF4342" }}> ask a local!</span></p>
-                        </div>
-                        <div className="px-4">
-                            <HomeFilters isChecked={isChecked} handleCheckboxChange={handleCheckboxChange} inputClear={inputClear} setInputClear={setInputClear} clearFilters={clearFilters} />
-                        </div>
-                    </div>
-                }
-                {!isMobile &&
-                    <>
-                        <div className="flex items-center ">
-                            <div className="w-1/3">
-                                <div className=" flex flex-col ">
-                                    <h1 className=" text-4xl font-fira_sans" style={{ color: "#0097B2" }}>Hungry? Thirsty?</h1>
-                                    <p className=" text-2xl leading-10 max-w-xs mt-3">If you want to know the best places to eat and drink...  <span className=" font-bold text-2xl" style={{ color: "#CF4342" }}> ask a local!</span></p>
-                                </div>
+                <div className="max-w-5xl md:mx-auto pb-10">
+                    {isMobile &&
+                        <div className=" flex flex-col gap-5">
+                            <div className=" rounded flex flex-col items-center mt-6">
+                                <h1 className=" text-3xl font-fira_sans" style={{ color: "#0097B2" }}>Hungry? Thirsty?</h1>
+                                <p className=" text-xl leading-10 max-w-xs mt-3">If you want to know the best places to eat and drink...  <span className=" font-bold text-2xl" style={{ color: "#CF4342" }}> ask a local!</span></p>
                             </div>
-                            <div className="flex w-1/3 flex-col pl-24">
-                                <div className="flex flex-col">
-                                    <p className="text-xl font-bold font-raleway" >Search Results:</p>
-                                    {numberOfPosts > 0 && allMembers ? (
-                                        <>
+                            <div className="px-4">
+                                <HomeFilters isChecked={isChecked} handleCheckboxChange={handleCheckboxChange} inputClear={inputClear} setInputClear={setInputClear} clearFilters={clearFilters} />
+                            </div>
+                        </div>
+                    }
+                    {!isMobile &&
+                        <>
+                            <div className="flex items-center ">
+                                <div className="w-1/3">
+                                    <div className=" flex flex-col ">
+                                        <h1 className=" text-4xl font-fira_sans" style={{ color: "#0097B2" }}>Hungry? Thirsty?</h1>
+                                        <p className=" text-2xl leading-10 max-w-xs mt-3">If you want to know the best places to eat and drink...  <span className=" font-bold text-2xl" style={{ color: "#CF4342" }}> ask a local!</span></p>
+                                    </div>
+                                </div>
+                                <div className="flex w-1/3 flex-col pl-24">
+                                    <div className="flex flex-col">
+                                        <p className="text-xl font-bold font-raleway" >Search Results:</p>
+                                        {numberOfPosts > 0 && allMembers ? (
+                                            <>
+                                                <p className="text-xl py-2 font-bold font-raleway" style={{ color: "#4e9da8" }}>
+                                                    {numberOfPosts}{" "}
+                                                    <span>Businesses</span>
+                                                </p>
+                                                <p className="text-xl py-2 font-bold font-raleway" style={{ color: "#4e9da8" }}>
+                                                    {allMembers?.length} <span>Members</span>
+                                                </p>
+                                            </>
+                                        ) : (
                                             <p className="text-xl py-2 font-bold font-raleway" style={{ color: "#4e9da8" }}>
-                                                {numberOfPosts}{" "}
-                                                <span>Businesses</span>
+                                                Loading...
                                             </p>
-                                            <p className="text-xl py-2 font-bold font-raleway" style={{ color: "#4e9da8" }}>
-                                                {allMembers?.length} <span>Members</span>
-                                            </p>
-                                        </>
-                                    ) : (
-                                        <p className="text-xl py-2 font-bold font-raleway" style={{ color: "#4e9da8" }}>
-                                            Loading...
-                                        </p>
-                                    )}
+                                        )}
 
+                                    </div>
+                                </div>
+                                <div className="w-1/3 flex justify-center">
+                                    <img src="images/cropped_logo.png" alt="" width={350} style={{ minHeight: "200px", minWidth: '200px' }} />
                                 </div>
                             </div>
-                            <div className="w-1/3 flex justify-center">
-                                <img src="images/cropped_logo.png" alt="" width={350} style={{ minHeight: "200px", minWidth: '200px' }} />
-                            </div>
-                        </div>
-                        <HomeFilters isChecked={isChecked} handleCheckboxChange={handleCheckboxChange} clearFilters={clearFilters} inputClear={inputClear} setInputClear={setInputClear} />
-                    </>
-                }
+                            <HomeFilters isChecked={isChecked} handleCheckboxChange={handleCheckboxChange} clearFilters={clearFilters} inputClear={inputClear} setInputClear={setInputClear} />
+                        </>
+                    }
+                </div>
                 <div className="flex justify-between">
                     <p className={`${isMobile ? "py-2 px-7" : ""}`}>
                         {startIndex} - {endIndex} of {!isFilter ? numberOfPosts : filterPosts.length} results
